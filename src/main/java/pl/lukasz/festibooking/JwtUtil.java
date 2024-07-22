1b8f5226c2be47f35d70d8c98dbc5f988e9da284
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2024-07-22 17:53:38 +0200

    added class(JwtUtil, LoginRequest, SecurityConfig)

---
+++ src/main/java/pl/lukasz/festibooking/JwtUtil.java
@@ -0,0 +1,22 @@
+package pl.lukasz.festibooking;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private String secretKey = "yourSecretKey";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
