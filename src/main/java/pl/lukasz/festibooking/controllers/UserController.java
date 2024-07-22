9b2cd6e2d9ae0b510c0fe583a36c89734749ed6a
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2024-07-22 17:52:41 +0200

    changes in properites, pom.xml, UserController, UserRepository

--- src/main/java/pl/lukasz/festibooking/controllers/UserController.java
+++ src/main/java/pl/lukasz/festibooking/controllers/UserController.java
@@ -1,7 +1,50 @@
 package pl.lukasz.festibooking.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.lukasz.festibooking.JwtUtil;
+import pl.lukasz.festibooking.LoginRequest;
+import pl.lukasz.festibooking.entities.User;
+import pl.lukasz.festibooking.repositories.UserRepository;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash()));
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginRequest loginRequest) {
+        Optional<User> userOptional = userRepository.findByEmail(loginRequest.getEmail());
+        if (userOptional.isPresent() && passwordEncoder.matches(loginRequest.getPassword(), userOptional.get().getPasswordHash())) {
+            String jwtToken = jwtUtil.generateToken(loginRequest.getEmail());
+            return ResponseEntity.ok(jwtToken);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        }
+    }
 }
--- src/main/java/pl/lukasz/festibooking/repositories/UserRepository.java
+++ src/main/java/pl/lukasz/festibooking/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.lukasz.festibooking.entities.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
